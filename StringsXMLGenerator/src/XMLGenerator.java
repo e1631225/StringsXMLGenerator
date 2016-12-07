04b85adef87deb184d16711c9327a11981203b35
Süleyman YILMAZ <suleymanyilmazs@gmail.com>
2016-12-07 15:45:32 -0300

    second test commit

--- StringsXMLGenerator/src/XMLGenerator.java
+++ StringsXMLGenerator/src/XMLGenerator.java
@@ -3,6 +3,7 @@ public class XMLGenerator {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
+		System.out.println("Test Commit");
 	}
 
 }
