42db13dab2137b85b3d6969eb2fb9ff504d9b8ef
Süleyman YILMAZ <suleymanyilmazs@gmail.com>
2016-12-09 19:27:57 -0300

    languages are obtained from server

--- StringsXMLGenerator/src/GsonResponse.java
+++ StringsXMLGenerator/src/GsonResponse.java
@@ -1,14 +1,27 @@
 import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.LinkedBlockingQueue;
 
 
-public class GsonResponse<T> {
+public class GsonResponse {
 //	String text;
-	LinkedHashMap<String, Language> text;
+	Map<String, Language> text = new LinkedHashMap<String, GsonResponse.Language>();
 	
 	
 	
+	public Map<String, Language> getText() {
+		return text;
+	}
+
+
+
+	public void setText(LinkedHashMap<String, Language> text) {
+		this.text = text;
+	}
+
+
+
 	public class Language {
 		String name;
 		String dir;
--- StringsXMLGenerator/src/HTTPRequestHandler.java
+++ StringsXMLGenerator/src/HTTPRequestHandler.java
@@ -2,6 +2,8 @@ import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 import javax.net.ssl.HttpsURLConnection;
 
@@ -79,8 +81,12 @@ public class HTTPRequestHandler {
 		System.out.println(result.toString());
 		
 		Gson gson = new Gson();
-		GsonResponse response1 = gson.fromJson(result.toString(), GsonResponse.class);
-		System.out.println(response1.toString());
+		GsonResponse gsonResult = gson.fromJson(result.toString(), GsonResponse.class);
+		Map<String, GsonResponse.Language> map = (Map<String, GsonResponse.Language>) gsonResult.getText();
+		for (String key : map.keySet()) {
+			System.out.println(key);
+		}
+		
 		
 		
 		
